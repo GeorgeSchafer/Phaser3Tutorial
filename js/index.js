503888e4e4939b34b3b9cf557232b94409cf5786
GeorgeSchafer <77408854+GeorgeSchafer@users.noreply.github.com>
2024-03-11 03:16:06 -0700

    updates to phaserhelpers

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ import {
     Config,
     Scene,
     View,
+    Player,
     Frame,
     SquareFrame,
     Img,
@@ -9,33 +10,40 @@ import {
     physics
 } from './PhaserHelpers/Constants.mjs'
 
-const assets = 'js/assets/'
-
+const assets = '../js/assets/'
 const view = new View(512,382)
-const elements = {
+const imgs = {
     background: new Img('sky', assets + 'sky.png'),
     star: new Img('star', assets + 'star.png'),
     platform: new Img('platform', assets + 'platform.png'),
     bomb: new Img('bomb', assets + 'bomb.png'),
-    dude: new SpriteSheet('dude', assets + 'dude.png', new SquareFrame(48))
 }
-const background = new Img('sky', 'assets/sky.png')
-const dude = new SpriteSheet('dude', assets + 'dude.png', new SquareFrame(48))
 
-const preload = () => {
-    // Object.values(elements).forEach(value => {
-    //     value.preload(this)
-    // })
-    this.load.image(background.alias, background.path)
+const dude = new Player(new SpriteSheet('dude', assets + 'dude.png', new Frame(32,48)))
+const sceneA = new Scene(preload, create, update)
+
+function preload()
+{
+    Object.values(imgs).forEach(value => {
+        value.preload(this)
+    })
     dude.preload(this)
 }
-const create = () => {
-    elements.background.create(this, view.width/2, view.height/2)
-    elements.star.create(this, 0,0)
-}
-const update = () => {}
 
-const sceneA = new Scene(preload, create, update)
+function create()
+{
+    Object.values(imgs).forEach(value => {
+        value.create(this, view.centerX(), view.centerY())
+    })
+    dude.create(view, this)
+
+}
+
+function update()
+{
+    
+}
+
 const config = new Config(view, physics.platformer, sceneA)
 const game = new Phaser.Game(config)
 
